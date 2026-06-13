9947b2470aca963dbdcfdfe364dadfc645a4464e
tanuj <tanujagarwal1825@gmail.com>
2026-06-13 15:44:28 +0530

    Add: permutations - submission-0

---
+++ Data Structures & Algorithms/permutations/submission-0.java
@@ -0,0 +1,27 @@
+class Solution {
+    public void recurPermute(int[] nums,List<List<Integer>> ans,List<Integer> ds, boolean[] freq ){
+        if(ds.size()==nums.length){
+            ans.add(new ArrayList<>(ds));
+            return;
+        }
+
+        for(int i=0;i<nums.length;i++){
+            if(!freq[i]){
+                freq[i]=true;
+                ds.add(nums[i]);
+                recurPermute(nums,ans,ds,freq);
+                ds.remove(ds.size()-1);
+                freq[i]=false;
+            }
+        }
+    }
+
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> ds = new ArrayList<>();
+        boolean[] freq = new boolean[nums.length];
+        recurPermute(nums,ans,ds,freq);
+
+        return ans;
+    }
+}
