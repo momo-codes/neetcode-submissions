8a5f32a462f5b658db7af6db9d424f994e17f547
tanuj <tanujagarwal1825@gmail.com>
2026-06-21 00:52:29 +0530

    Add: sql-sellers-with-no-sales - submission-4

---
+++ SQL/sql-sellers-with-no-sales/submission-4.sql
@@ -0,0 +1,11 @@
+-- Write your query below
+SELECT seller_name 
+FROM seller 
+WHERE seller_id NOT IN (
+    SELECT seller_id 
+    FROM orders 
+    WHERE EXTRACT(YEAR FROM sale_date) = 2020
+) ORDER BY seller_name;
+
+
+
