f90de8ceb25ddc40f5e09eda62fa85e11e30e849
tanuj <tanujagarwal1825@gmail.com>
2026-07-02 00:55:25 +0530

    Add: find-k-closest-elements - submission-1

---
+++ Data Structures & Algorithms/find-k-closest-elements/submission-1.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+        PriorityQueue<Integer> q = new PriorityQueue<>((a,b)->{
+            int ac = Math.abs(a-x);
+            int bc = Math.abs(b-x);
+            if(ac==bc){
+                return b-a;
+            }
+            return bc-ac;
+            });
+        for(int a:arr){
+            q.add(a);
+            if(q.size()>k){
+                q.remove();
+            }
+        }
+        List<Integer> ans = new ArrayList<>();
+        while(!q.isEmpty()){
+            ans.add(q.remove());
+        }
+       ans.sort((a,b)->a-b);
+       return ans;
+    }
+}
