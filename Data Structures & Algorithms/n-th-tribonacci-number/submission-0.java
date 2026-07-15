1459ef1d79a81820d3cd7ad8f5701a8384f986f6
tanuj <tanujagarwal1825@gmail.com>
2026-07-16 00:38:26 +0530

    Add: n-th-tribonacci-number - submission-0

---
+++ Data Structures & Algorithms/n-th-tribonacci-number/submission-0.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int trib(int n,int[] dp){
+        if(n==0){
+            return 0;
+        }
+        if(n<=2){
+            return 1;
+        }
+        if(dp[n]!=-1) return dp[n];
+
+        return dp[n] = trib(n-1,dp) + trib(n-2,dp) + trib(n-3,dp);
+    }
+    public int tribonacci(int n) {
+        int[] dp =new int[n+1];
+        Arrays.fill(dp,-1);
+        return trib(n,dp);
+    }
+}
