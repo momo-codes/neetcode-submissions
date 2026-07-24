ab84a4742c01841406c15bebfee68d19003945e7
tanuj <tanujagarwal1825@gmail.com>
2026-07-24 20:34:59 +0530

    Add: pascals-triangle - submission-0

---
+++ Data Structures & Algorithms/pascals-triangle/submission-0.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+    List<List<Integer>> ans = new ArrayList<>();
+    for(int i=1;i<=numRows;i++){
+        List<Integer> temp = new ArrayList<>();
+        for(int j = 0;j<i;j++){
+            if(j==0 || j==i-1){
+                temp.add(1);
+            } 
+            else{
+                temp.add(ans.get(i-2).get(j-1) + ans.get(i-2).get(j));
+            }
+        }
+        ans.add(new ArrayList<>(temp));
+    }
+    return ans;
+    }
+}
